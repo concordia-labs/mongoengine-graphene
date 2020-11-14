40a591f3570993015237d019667aaf934260b94f
GabrielTaufer <taufergabrielangelo@gmail.com>
2020-11-14 10:03:47 -0300

    Configure repository to be downloaded with pip

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='mongoengine_graphene',
+    version='1.0',
+    description='Mongoengine Graphene',
+    author='Lucas Soranzo',
+    author_email='lucasdeonsoranzo@gmail.com',
+    url='https://github.com/lucas-soranzo/mongoengine-graphene',
+    packages=[
+        'mongoengine_graphene',
+    ],
+)
+
